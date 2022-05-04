22ade2c75c9f6d4e87ef194ab43ea3ba2525f95a
zxlfly <zxlfly@foxmail.com>
2022-05-05 00:12:01 +0800

    up

--- ffmpeg/src/download.js
+++ ffmpeg/src/download.js
@@ -2,73 +2,75 @@ import { exec, execSync } from 'child_process'
 import fs from 'fs'
 import readline from 'readline'
 
-const array = []
+(async function () {
+  const array = []
 
-async function processLineByLine() {
-  const fileStream = fs.createReadStream('allText.txt');
+  async function processLineByLine() {
+    const fileStream = fs.createReadStream('allText.txt');
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  // 注意：使用 crlfDelay 选项
-  // 将 input.txt 中的所有 CR LF ('\r\n') 实例识别为单个换行符。
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+    // 注意：使用 crlfDelay 选项
+    // 将 input.txt 中的所有 CR LF ('\r\n') 实例识别为单个换行符。
 
-  for await (const line of rl) {
-    // input.txt 中的每一行都将在此处作为 `line` 连续可用。
-    if (line.length > 10) {
-      array.push(line)
-    }
-    // console.log(`Line from file: ${line}`);
-  }
-  //   console.log(array.length);
-}
-
-await processLineByLine()
-console.log(array);
-console.log(array.length);
-const errorList = []
-await sendRequest(array, 10)
-console.log('完成！,失败任务：', errorList);
-async function sendRequest(req, concurrent = 0) {
-  return new Promise((resolve, reject) => {
-    let len = req.length;
-    let cut = 0;
-
-    const start = async () => {
-      // 从头部弹出任务
-      let task = req.shift();
-      // 执行任务
-      if (task) {
-        try {
-          console.log('开始-----：', task.split('aac_adtstoasc')[1])
-          exec(task, (error, stdout, stderr) => {
-            if (error) {
-              console.error(`exec error: ${error}`);
-              return;
-            }
-            if (cut == len - 1) {
-              console.log('完成-----：', task.split('aac_adtstoasc')[1])
-              resolve();
-            } else {
-              console.log('完成-----：', task.split('aac_adtstoasc')[1])
-              cut++;
-              start();
-            }
-          })
-        } catch (e) {
-          // 可以重新下载，这里跳过了
-          cut++
-          start()
-          errorList.push(task)
-          console.error('失败任务：', task.split('aac_adtstoasc')[1]);
-        }
+    for await (const line of rl) {
+      // input.txt 中的每一行都将在此处作为 `line` 连续可用。
+      if (line.length > 10) {
+        array.push(line)
       }
-    };
-    while (concurrent > 0) {
-      start();
-      concurrent--;
+      // console.log(`Line from file: ${line}`);
     }
-  });
-}
+    //   console.log(array.length);
+  }
+
+  await processLineByLine()
+  console.log(array);
+  console.log(array.length);
+  const errorList = []
+  await sendRequest(array, 10)
+  console.log('完成！,失败任务：', errorList);
+  async function sendRequest(req, concurrent = 0) {
+    return new Promise((resolve, reject) => {
+      let len = req.length;
+      let cut = 0;
+
+      const start = async () => {
+        // 从头部弹出任务
+        let task = req.shift();
+        // 执行任务
+        if (task) {
+          try {
+            console.log('开始-----：', task.split('aac_adtstoasc')[1])
+            exec(task, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+              if (cut == len - 1) {
+                console.log('完成-----：', task.split('aac_adtstoasc')[1])
+                resolve();
+              } else {
+                console.log('完成-----：', task.split('aac_adtstoasc')[1])
+                cut++;
+                start();
+              }
+            })
+          } catch (e) {
+            // 可以重新下载，这里跳过了
+            cut++
+            start()
+            errorList.push(task)
+            console.error('失败任务：', task.split('aac_adtstoasc')[1]);
+          }
+        }
+      };
+      while (concurrent > 0) {
+        start();
+        concurrent--;
+      }
+    });
+  }
+})()
 
--- ffmpeg/src/index.js
+++ ffmpeg/src/index.js
@@ -1,94 +1,94 @@
 import fs from 'fs'
 import path from 'path'
 import axios from 'axios'
-const checkPath = async function (path) {
-  try {
-    await fs.promises.access(path)
-  } catch {
-    fs.mkdirSync(path)
+(async function () {
+  const checkPath = async function (path) {
+    try {
+      await fs.promises.access(path)
+    } catch {
+      fs.mkdirSync(path)
+    }
+    return true
   }
-  return true
-}
 
-const basePath = './video'
-await checkPath(basePath)
+  const basePath = './video'
+  await checkPath(basePath)
 
 
-const config = {
-  headers:
-  {
-    Authorization: '',
-    cookie: ''
+  const config = {
+    headers:
+    {
+      Authorization: '',
+      cookie: ''
+    }
   }
-}
 
-const courseUrl = ''
-const chapterUrl = ''
-const mediaUrl = ''
+  const courseUrl = ''
+  const chapterUrl = ''
+  const mediaUrl = ''
 
-const accessToken = ''
+  const accessToken = ''
 
-const { data: { data: courseInfo } } = await axios.get(courseUrl, config)
-const chapterList = courseInfo.chapter_list
-console.log(chapterList);
+  const { data: { data: courseInfo } } = await axios.get(courseUrl, config)
+  const chapterList = courseInfo.chapter_list
+  console.log(chapterList);
 
 
-let allText = ''
-console.log(chapterList.length);
-for (let i = 0; i < chapterList.length; i++) {
+  let allText = ''
+  console.log(chapterList.length);
+  for (let i = 0; i < chapterList.length; i++) {
 
-  const chapterName = `${i + 1}、${chapterList[i].chapter_name}`
-  const chapterPath = `${basePath}/${chapterName}`
-  console.log(await checkPath(chapterPath))
-  const url = chapterUrl + chapterList[i].chapter_id
-  const { data: { data: chapterInfo } } = await axios.get(url, config)
-  // console.log(chapterInfo);
+    const chapterName = `${i + 1}、${chapterList[i].chapter_name}`
+    const chapterPath = `${basePath}/${chapterName}`
+    console.log(await checkPath(chapterPath))
+    const url = chapterUrl + chapterList[i].chapter_id
+    const { data: { data: chapterInfo } } = await axios.get(url, config)
+    // console.log(chapterInfo);
 
-  const sectionList = chapterInfo.section_list
-  for (let j = 0; j < sectionList.length; j++) {
+    const sectionList = chapterInfo.section_list
+    for (let j = 0; j < sectionList.length; j++) {
 
-    // console.log(sectionList[0].group_list[0].content_list[0].content.length);
-    const groupInfo = sectionList[j].group_list[0]
-    let name = groupInfo.group_name.replace(/\//g, '-')
-    const groupName = `${j + 1}、${name}`
-    const groupPath = `${chapterPath}/${groupName}`
-    console.log(await checkPath(groupPath))
+      // console.log(sectionList[0].group_list[0].content_list[0].content.length);
+      const groupInfo = sectionList[j].group_list[0]
+      let name = groupInfo.group_name.replace(/\//g, '-')
+      const groupName = `${j + 1}、${name}`
+      const groupPath = `${chapterPath}/${groupName}`
+      console.log(await checkPath(groupPath))
 
-    const contentList = groupInfo.content_list
-    const fileName = groupPath + '/' + name + '.txt'
-    for (let k = 0; k < contentList.length; k++) {
-      // if(k) break
-      const { content: contents, content_type, content_title } = contentList[k]
+      const contentList = groupInfo.content_list
+      const fileName = groupPath + '/' + name + '.txt'
+      for (let k = 0; k < contentList.length; k++) {
+        // if(k) break
+        const { content: contents, content_type, content_title } = contentList[k]
 
-      if (content_type === 3 || content_type === 7) {
-        // fs.rmSync(fileName)
-        let contentText = ''
-        for (let l = 0; l < contents.length; l++) {
-          const { callback_key: mediaId } = contents[l]
-          const params = { mediaId, accessToken }
-          const { data: { data: videoInfo } } = await axios.get(mediaUrl, { ...config, params })
-          const { playURL } = videoInfo.mediaMetaInfo.videoGroup[0]
-          console.log(playURL);
-          contentText += `ffmpeg -i ${playURL} -c copy -bsf:a aac_adtstoasc ./${content_title}--${l < 9 ? 0 : ''}${l + 1}.mp4\n`
-          allText += `ffmpeg -i ${playURL} -c copy -bsf:a aac_adtstoasc "${path.resolve(groupPath)}/${content_title}--${l < 9 ? 0 : ''}${l + 1}.mp4"\n`
+        if (content_type === 3 || content_type === 7) {
+          // fs.rmSync(fileName)
+          let contentText = ''
+          for (let l = 0; l < contents.length; l++) {
+            const { callback_key: mediaId } = contents[l]
+            const params = { mediaId, accessToken }
+            const { data: { data: videoInfo } } = await axios.get(mediaUrl, { ...config, params })
+            const { playURL } = videoInfo.mediaMetaInfo.videoGroup[0]
+            console.log(playURL);
+            contentText += `ffmpeg -i ${playURL} -c copy -bsf:a aac_adtstoasc ./${content_title}--${l < 9 ? 0 : ''}${l + 1}.mp4\n`
+            allText += `ffmpeg -i ${playURL} -c copy -bsf:a aac_adtstoasc "${path.resolve(groupPath)}/${content_title}--${l < 9 ? 0 : ''}${l + 1}.mp4"\n`
+          }
+          contentText += '\n'
+          allText += '\n'
+          fs.writeFileSync(fileName, contentText, { flag: 'a+' })
         }
-        contentText += '\n'
-        allText += '\n'
-        fs.writeFileSync(fileName, contentText, { flag: 'a+' })
-      }
 
-      if (content_type === 6) {
-        for (let l = 0; l < contents.length; l++) {
-          const { name, url } = contents[l]
-          const file = groupPath + '/' + name
-          const writer = fs.createWriteStream(file)
-          const response = await axios({ url, responseType: 'stream' })
-          response.data.pipe(writer)
+        if (content_type === 6) {
+          for (let l = 0; l < contents.length; l++) {
+            const { name, url } = contents[l]
+            const file = groupPath + '/' + name
+            const writer = fs.createWriteStream(file)
+            const response = await axios({ url, responseType: 'stream' })
+            response.data.pipe(writer)
+          }
         }
       }
     }
   }
-}
-fs.writeFileSync('./video/allText.txt', allText)
-
-
+  fs.writeFileSync('./video/allText.txt', allText)
+})()
